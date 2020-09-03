98a95f124f5fe0b9587b96b877cad5f340358b3f
Jacqueline <lojacqueline2@gmail.com>
2020-09-03 02:45:08 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -81,7 +83,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+	programmer_hash [:yukihiro_matsumoto] = {   
+  :known_for => "Ruby",
+ :languages => ["LISP", "C"]
+}
 
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +110,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+ alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for]= alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +135,11 @@ def adding_to_dennis
       }
     }
 
+  if programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
+else 
+  programmer_hash = "Assembly"
+  programmer_hash
+end
 end
